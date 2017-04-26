7a4112fdff902e57df56aedbf5eb699f137e9935
kosta <kosta@kosta-PC>
2017-04-26 18:25:43 +0900

    boram product update

--- WebContent/ProductDeal/product_registerOk.jsp
+++ WebContent/ProductDeal/product_registerOk.jsp
@@ -1,5 +1,11 @@
+<%@page import="travel.product.model.ProductDao"%>
 <%@ page language="java" contentType="text/html; charset=EUC-KR"
     pageEncoding="EUC-KR"%>
+    <%
+	request.setCharacterEncoding("utf-8");
+%>
+
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
--- src/travel/product/action/insertAction.java
+++ src/travel/product/action/insertAction.java
@@ -1,8 +1,6 @@
 package travel.product.action;
 
-import java.io.IOException;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -15,19 +13,35 @@ public class insertAction implements Action{
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception  {
 		ProductDao dao = ProductDao.getInstance();
 		Product product = new Product();
-		product.setP_num(Integer.parseInt(request.getParameter("p_num")));
+		product.setP_num(request.getParameter("p_num"));
+		
+		
 		product.setU_id(request.getParameter("u_id"));
+	
+		
 		product.setP_img(request.getParameter("p_img"));
+	
+		
 		product.setP_detail(request.getParameter("p_detail"));
-		product.setP_price(Integer.parseInt(request.getParameter("p_price")));
+		
+		
+		product.setP_price(request.getParameter("p_price"));
+		
+		
+		product.setP_amount(request.getParameter("p_amount"));
+		
+		
 		product.setP_term(request.getParameter("p_term"));
+		
+		
 		product.setP_ox(request.getParameter("p_ox"));
+		
 		
 		dao.insertProduct(product);
 		
 		ActionForward forward = new ActionForward();
 		forward.setRedirect(true);
-		forward.setPath("/ProductDeal/product_registerOk.jsp");
+		forward.setPath("ProductDeal/product_registerOk.jsp");
 	
 		
 		return forward;
