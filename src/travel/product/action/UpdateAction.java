f0160ec766d0619fca6b78c23509ef2b125f514b
kosta <kosta@kosta-PC>
2017-04-28 18:08:49 +0900

    boram 170428 commit

--- src/travel/product/action/UpdateAction.java
+++ src/travel/product/action/UpdateAction.java
@@ -1,11 +1,16 @@
 package travel.product.action;
 
+import java.io.File;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
+import travel.product.model.ImageUtil;
 import travel.product.model.Product;
 import travel.product.model.ProductDao;
 
@@ -15,14 +20,21 @@ public class UpdateAction implements Action {
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException, Exception {
 		
-		String p_num = request.getParameter("p_num");
-
+		String uploadPath = request.getRealPath("upload");
+		int size = 20 * 1024 * 1024; //20MB
+		
+		MultipartRequest multi = new MultipartRequest(request, uploadPath, size, 
+				"utf-8", new DefaultFileRenamePolicy());
+		
+		String p_num = multi.getParameter("p_num");
+		String p_img = multi.getFilesystemName("p_img");
+		
 		System.out.println(p_num);
-
+		System.out.println(p_img);
 		
 		ProductDao dao = ProductDao.getInstance();
 		Product product = dao.detailProduct(p_num);
-		
+
 		product.setP_num(p_num);
 
 		dao.updateProduct(product);
