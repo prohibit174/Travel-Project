eef42a1fe550d90ca7158448044a17219884c16d
KIMDUDU <ponic2@naver.com>
2017-04-28 10:56:26 +0900

    로그인 팝업 띄우는것 완료

---
+++ WebContent/JS/loginPopup/loginPopup.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.btn-new-plan').click(function(){
+		var popUrl = "login.html";
+		var popOption = "width= 370, height=360, resizlble=no, scrollbars=no, status=no;";
+		window.open(popUrl, "" , popOption);
+	});
+});
+	
