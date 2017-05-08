5f87aa7ac612cd02469f7f02a638128f9f7c9574
kosta <kosta@kosta-PC>
2017-05-08 14:56:58 +0900

    BORAM PAGING

--- src/travel/product/mapper/TravelMapper.java
+++ src/travel/product/mapper/TravelMapper.java
@@ -2,6 +2,9 @@ package travel.product.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.session.RowBounds;
+
+
 import travel.product.model.Product;
 import travel.product.model.Product_Request;
 import travel.product.model.Product_Search;
@@ -9,13 +12,15 @@ import travel.product.model.Product_Search;
 public interface TravelMapper {
 
 	int insertProduct(Product product);
-	List<Product> listProduct(Product_Search proSearch);
+	List<Product> listProduct(RowBounds row,Product_Search search);
 	Product detailProduct(String p_num);
 	int updateProduct(Product product);
 	int deleteProduct(Product product);
 	Product_Request detailProductReq(String p_num);
 	int insertProductReq(Product_Request proReq);
 	List<Product_Request> listProductRequest(String u_id);
+	int countProduct(Product_Search proSearch);
+	List<Product> listProduct(Product_Search proSearch);
 
 
 }
